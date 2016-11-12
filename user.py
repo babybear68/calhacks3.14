db43b2c144465c34023a117d677eb73a234c9882
dingyiming0427 <dingyiming0427@berkeley.edu>
2016-11-12 04:03:52 -0800

    user

--- user.py
+++ user.py
@@ -1,18 +1,18 @@
-def User:
-    def __init__(self, username, password, year, age, gender, major=None, interest=None, personality=None, contact=None, \
-        basketball, football, ):
+class User:
+    def __init__(self, username, year, age, gender, major=[], interest=[], personality=[], contact=None, \
+        basketball, football, soccer, tennis):
         self.username = username #string
-        self.password = password #string
         self.year = year #string
         self.age = age #int
         self.gender = gender #string
-        self.major = major #string
-        self.interest = interest #string
-        self.personality = personality #string
+        self.major = major #list of string
+        self.interest = interest #list of string
+        self.personality = personality #list of string
         self.contact = contact #string
 
         self.go_events = []
         self.create_events = []
+        self.label_dic = {} #map the name of a label to the label instance
     """def go_to_event(self, event):
         if len(event.participants) > event.num_limit:
             #print ("Sorry you can't join; the limit is %s people" % (event.num_limit))
