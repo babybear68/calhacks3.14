0daaf7334725245ebe7361a67c5a14d351dba055
babybear68 <eternalbabybear@aim.com>
2016-11-12 22:12:48 -0800

    update

--- user-creation/create.py
+++ user-creation/create.py
@@ -3,6 +3,27 @@ from flask import request
 from flask import render_template
 import sqlite3
 
+def decolon(st):
+    result = [[]]
+    ir = 0
+    for i in range(len(st)):
+        if st[i] == ';':
+            continue
+        elif st[i + 1] == ';':
+            result[ir].append(st[i])
+            result[ir] = ''.join(result[ir])
+            result.append([])
+            ir = ir + 1
+        else:
+            result[ir].append(st[i])
+    return result[:len(result) - 1]
+
+
+def encode(lst):
+    result = chr(31).join(lst)
+    result = result + chr(31)
+    return result
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,21 +32,21 @@ def create_user():
 
 @app.route('/', methods=['POST'])
 def create_user_post():
-	name = request.form['name']
-	age = request.form['age']
-	if request.form['sex'] == 'm':
-		sex = 'Male'
-	else:
-		sex = 'Female'
-	year = request.form['year']
-	major = request.form['major']
-	st = 'INSERT INTO users VALUES("' + name + '", '+ str(age) + ', "' + sex + '", ' + str(year) + ', "' + major + '")'
+    name = request.form['name']
+    year = request.form['year']
+    age = request.form['age']
+    gender = request.form['gender']
+    major = decolon(request.form['major'])
+    interest = decolon(request.form['interest'])
+    personality = decolon(request.form['personality'])
+    contact = request.form['contact']
+    st = 'INSERT INTO users VALUES("' + name + '", "'+ year + '", ' + str(age) + ', "' + gender + '", "' + encode(major) + '", "' + encode(interest) + '", "' + encode(personality) + '", "' + contact + '")'
 
-	with sqlite3.connect("users.db") as connection:
-		c = connection.cursor()
-		c.execute(st)
+    with sqlite3.connect("users.db") as connection:
+        c = connection.cursor()
+        c.execute(st)
 
-	return 'User ' + name + ' created successfully!'
+    return 'User ' + name + ' created successfully!'
 
 if __name__ == '__main__':
     app.run()
--- user-creation/create_db.py
+++ user-creation/create_db.py
@@ -2,4 +2,4 @@ import sqlite3
 
 with sqlite3.connect("users.db") as connection:
 	c = connection.cursor()
-	c.execute("""CREATE TABLE users(name TEXT, age INTEGER, sex TEXT, year INTEGER, major TEXT)""")
+	c.execute("""CREATE TABLE users(name TEXT, year TEXT, age INTEGER, gender TEXT, major TEXT, interest TEXT, personality TEXT, contact TEXT, labels TEXT)""")
