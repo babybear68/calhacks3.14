9bf83c09754d4e02a3014b33354e68bcec466f74
henryang <1123482074@qq.com>
2016-11-12 19:00:56 -0800

    More Label Classes
    Art, Food, and public events

--- backend/Calculator.py
+++ backend/Calculator.py
@@ -1,6 +1,11 @@
 from Label import *
 class Calculator:
-    weights_of_label_tpypes = {Basketball.name:Basketball.weights, Football.name:Football.weights, Soccer.name: Soccer.weights, Tennis.name:Tennis.weights}
+    weights_of_label_tpypes = \
+        {Basketball.name:Basketball.weights, Football.name:Football.weights, Soccer.name: Soccer.weights, Tennis.name:Tennis.weights,
+         Music.name: Music.weights, ReadingAndWriting.name: ReadingAndWriting.weights, Painting.name: Painting.weights, Poetry.name: Poetry.weights,
+         Concert.name: Concert.weights, Exhibition.name: Exhibition.weights, Game.name: Game.weights, Talk.name: Talk.weights,
+         American.name: American.weights, Mexican.name: Mexican.weights, Asian.name: Asian.weights, Mediterranean.name: Mediterranean.weights
+         }
 
     def get_value(user, key):
         if key in user.labels:
--- backend/Test_run.py
+++ backend/Test_run.py
@@ -15,11 +15,14 @@ soccer_low = 1/3
 user1.labels['basketball'] = Basketball.get_value('low')
 user2.labels['basketball'] = basketball_med
 user2.labels['baseball'] = soccer_low
+user2.labels['music'] = Music.levels['little']
 
 basketball_event = Event(user1,'lol', ('basketball',basketball_low), None, None, '', (1,3))
+music_event = Event(user1,'lol', ('music', 2/3), None, None, '', (1,3))
+
 
 print(user1.labels)
 print(user2.labels)
 print(user1 is user2)
 
-print(Calculator.calculate(basketball_event,user2))
+print(Calculator.calculate(music_event,user2))
