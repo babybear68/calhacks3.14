809d0908fde27f4ef05253eccc968a51f9723bc8
Yiming Ding <dingyiming0427@berkeley.edu>
2017-02-06 20:17:29 -0800

    deleted everything except backend

--- README.md
+++
@@ -1,3 +0,0 @@
-# calhacks3.14
-a simple cal hacks 3.0 project
-designed by Elena, Henry, Yiming, & Zekai in California.
--- event-creation/create_db.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-with sqlite3.connect("events.db") as connection:
-	c = connection.cursor()
-	c.execute("""CREATE TABLE users(name TEXT, num INTEGER, dt TEXT, type TEXT)""")
